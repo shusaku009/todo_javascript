8f32c3de65d85ea56c67298efad0279212499577
shusaku009 <shusaku009@gmail.com>
2023-10-25 20:48:04 +0900

    add: 入力内容をTodoリストに表示する

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-console.log("App.js: loaded");
+import { element, render } from "./view/html-util.js";
+
 export class App {
   mount() {
     const formElement = document.querySelector("#js-form");
     const inputElement = document.querySelector("#js-form-input");
+    const containerElement = document.querySelector("#js-todo-list");
+    const todoItemCountElement = document.querySelector("#js-todo-count");
+    // TodoリストをまとめるList要素
+    const todoListElement = element`<ul></ul>`;
+    // Todoアイテム数
+    let todoItemCount = 0;
     formElement.addEventListener("submit", (event) => {
       // submitイベントの本来の動作を止める
       event.preventDefault();
-      console.log(`入力欄の値: ${inputElement.value}`);
+      // 追加するTodoアイテムの要素(li要素)を作成する
+      const todoItemElement = element`<li>${inputElement.value}</li>`;
+      // TodoアイテムをtodoListElementに追加する
+      todoListElement.appendChild(todoItemElement);
+      // コンテナ要素の中身をTodoリストをまとめるList要素を上書きする
+      render(todoListElement, containerElement);
+      todoItemCount += 1;
+      todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+      // 入力欄を空文字にしてリセットする
+      inputElement.value = "";
     });
   }
 }
